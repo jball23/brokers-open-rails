12150ce5a6b51d3d6e0923e7913eaf8fa99d1fdc
Jordan Ballard <ballard.jordan@gmail.com>
2016-05-06 14:12:24 -0400

    added CRUD actions in users and listings controllers

--- brokers-open/app/controllers/listings_controller.rb
+++ brokers-open/app/controllers/listings_controller.rb
@@ -1,23 +1,35 @@
 class ListingsController < ApplicationController
 
   def index
+    @listings = Listing.all
+    render json: @listings
   end
 
   def show
+    @listing = Listing.find(params[:id])
+    render json: @listing
   end
 
   def create
+    @listing = Listing.create!(listing_params)
+    render json: @listing
   end
 
   def update
+    @listing = Listing.find(params[:id])
+    @listing.update!(listing_params)
+    render json: @listing
   end
 
   def destroy
+    @listing = Listing.find(params[:id])
+    @listing.destroy
+    render nothing:true
   end
 
   private
   def listing_params
-    params.require(:entry).permit(:date, :time_start, :time_end, :address, :city, :state, :zipcode,
+    params.require(:listing).permit(:date, :time_start, :time_end, :address, :city, :state, :zipcode,
                                   :county, :list_price, :property_status, :property_type, :mls_num,
                                   :bedrooms_num, :full_baths, :half_baths, :sponsors, :property_description,
                                   :food_offered, :food_description, :incentive, :property_image)
--- brokers-open/app/controllers/users_controller.rb
+++ brokers-open/app/controllers/users_controller.rb
@@ -1,22 +1,35 @@
 class UsersController < ApplicationController
 
   def index
+    @users = User.all
+    render json: @users
   end
 
   def show
+    @user = User.find(params[:id])
+    render json: @user
   end
 
   def create
+    @user = User.create!(user_params)
+    render json: @user
   end
 
   def update
+    @user = User.find(params[:id])
+    @user.update!(user_params)
+    render json: @user
   end
 
   def destroy
+    @user = User.find(params[:id])
+    @user.destroy
+    render nothing:true
   end
 
   private
-  def listing_params
-    params.require(:entry).permit(:first_name, :last_name, :email, :phone, :agency, :avatar)
+  def user_params
+    params.require(:user).permit(:first_name, :last_name, :email, :phone, :agency, :avatar)
+  end
 
 end
