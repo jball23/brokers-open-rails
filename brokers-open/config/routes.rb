5a43227d57fd96ebadb7d6fc7f1471d42819f8e9
Lauren C. Kennedy <laurenckennedy@gmail.com>
2016-05-10 11:46:30 -0400

    removed nested routes in rails api

--- brokers-open/config/routes.rb
+++ brokers-open/config/routes.rb
@@ -1,9 +1,6 @@
 Rails.application.routes.draw do
 
-get "/listings"     => "listings#index"
-get "/listings/:id" => "listings#show"
-
-  resources :users do
-    resources :listings
-  end
+  resources :users
+  resources :listings
+  
 end
